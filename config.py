a440e90eee682af21368b7d7d4a185695aea67dd
BratIgnat <86632889+BratIgnat@users.noreply.github.com>
2025-07-22 16:47:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+from dotenv import load_dotenv
+load_dotenv()  # это загрузит .env в переменные окружения
+
 import os
 
-BOT_TOKEN = os.getenv("7735459605:AAFUBeYIL7diOTEkZqCdz2xC06QkCOPNlv0")
-SUPABASE_URL = os.getenv("https://fiuebssdgqiqxmnrlgej.supabase.co")
-SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpdWVic3NkZ3FpcXhtbnJsZ2VqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwMTYzMTksImV4cCI6MjA2ODU5MjMxOX0.KbgUrxvepoH76dIpiAVs6-QQcvxHcb6RkU3ifP5IaBQ")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 if not all([BOT_TOKEN, SUPABASE_URL, SUPABASE_KEY]):
     raise EnvironmentError("Одна или несколько переменных окружения не заданы")
